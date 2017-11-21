0f2d77a4921c597f4dbc42d32c4b13822c2d997a
piplongrun <piplongrun@users.noreply.github.com>
2017-11-21 17:58:58 +0100

    Update __init__.py

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,8 +1,6 @@
-VERSION = '2.9'
-API_URL = 'https://api.tadata.me/imdb2ta/v2/?imdb_id=%s' # %s = imdb id
-
-POST_URL = 'https://traileraddict.cache.tadata.me/ajax/film_popular.php'
-POST_BODY = 'page=%d&filmid=%s' # %d = page, %s = Trailer Addict movie id
+VERSION = '2.10'
+API_URL = 'https://api.tadata.me/imdb2ta/v2/?imdb_id=%s'
+TAC_URL = 'https://traileraddict.cache.tadata.me/%s'
 
 TYPE_ORDER = ['trailer', 'feature_trailer', 'theatrical_trailer', 'behind_the_scenes', 'interview', 'deleted_scene']
 TYPE_MAP = {
@@ -19,10 +17,6 @@ def Start():
 
 	HTTP.CacheTime = CACHE_1WEEK
 	HTTP.Headers['User-Agent'] = 'Trailer Addict/%s (%s %s; Plex Media Server %s)' % (VERSION, Platform.OS, Platform.OSVersion, Platform.ServerVersion)
-
-	if 'movies' in Dict:
-		Dict['movies'] = {}
-		Dict.Save()
 
 ####################################################################################################
 class TrailerAddictAgent(Agent.Movies):
@@ -64,7 +58,7 @@ class TrailerAddictAgent(Agent.Movies):
 	def update(self, metadata, media, lang):
 
 		try:
-			json_obj = JSON.ObjectFromURL(API_URL % (metadata.id), sleep=2.0)
+			json_obj = JSON.ObjectFromURL(API_URL % (metadata.id))
 		except:
 			Log("*** Failed retrieving data from %s ***" % (API_URL % (metadata.id)))
 			return None
@@ -73,66 +67,54 @@ class TrailerAddictAgent(Agent.Movies):
 			Log("*** An error occurred: %s ***" % (json_obj['error']))
 			return None
 
-		ta_movie_id = json_obj['ta_id']
-
-		if not ta_movie_id:
-			return None
-
 		poster = json_obj['image'] if 'image' in json_obj else None
 		extras = []
 
-		for page in range(1,2):
+		try:
+			html = HTML.ElementFromURL(TAC_URL % (json_obj['url'].split('/')[-1]), sleep=5.0)
+		except:
+			Log("*** HTTP GET request failed; plugin version %s ***" % (VERSION))
+			return None
 
-			try:
-				page = HTTP.Request(POST_URL, data=POST_BODY % (page, ta_movie_id), sleep=2.0).content # Only HTTP.Request supports a POST body
-			except:
-				Log("*** POST request failed; plugin version %s ***" % (VERSION))
-				break
+		for video in html.xpath('//a[@class="m_title"]'):
 
-			html = HTML.ElementFromString(page)
+			title = video.text.strip()
+			url = video.get('href')
 
-			if len(html.xpath('//a')) < 1:
-				break
+			# Trailers
+			if title.lower() == 'trailer' and Prefs['add_trailers']:
+				extra_type = 'trailer'
+			elif title.lower() == 'feature trailer' and Prefs['add_feature_trailers']:
+				extra_type = 'feature_trailer'
+			elif title.lower() == 'theatrical trailer' and Prefs['add_theatrical_trailers']:
+				extra_type = 'theatrical_trailer'
 
-			for video in html.xpath('//a'):
+			# Behind the scenes / Featurette
+			elif ('behind the scenes' in title.lower() or 'featurette' in title.lower()) and Prefs['add_featurettes']:
+				extra_type = 'behind_the_scenes'
 
-				title = video.text
-				url = video.get('href')
+			# Interview
+			elif 'interview' in title.lower() and Prefs['add_interviews']:
+				extra_type = 'interview'
 
-				# Trailers
-				if title.lower() == 'trailer' and Prefs['add_trailers']:
-					extra_type = 'trailer'
-				elif title.lower() == 'feature trailer' and Prefs['add_feature_trailers']:
-					extra_type = 'feature_trailer'
-				elif title.lower() == 'theatrical trailer' and Prefs['add_theatrical_trailers']:
-					extra_type = 'theatrical_trailer'
+				if title.lower().startswith('interview') or title.lower().startswith('generic interview'):
+					title = title.split('nterview - ')[-1].split('nterview- ')[-1]
 
-				# Behind the scenes / Featurette
-				elif ('behind the scenes' in title.lower() or 'featurette' in title.lower()) and Prefs['add_featurettes']:
-					extra_type = 'behind_the_scenes'
+			# Deleted scene
+			elif 'deleted scene' in title.lower() and Prefs['add_deleted_scenes']:
+				extra_type = 'deleted_scene'
 
-				# Interview
-				elif 'interview' in title.lower() and Prefs['add_interviews']:
-					extra_type = 'interview'
+			else:
+				continue
 
-					if title.lower().startswith('interview') or title.lower().startswith('generic interview'):
-						title = title.split('nterview - ')[-1].split('nterview- ')[-1]
-
-				# Deleted scene
-				elif 'deleted scene' in title.lower() and Prefs['add_deleted_scenes']:
-					extra_type = 'deleted_scene'
-
-				else:
-					continue
-
-				extras.append({
-					'type': extra_type,
-					'extra': TYPE_MAP[extra_type](
-						url = 'ta://%s' % (url.lstrip('/')),
-						title = title,
-						thumb = poster
-					)
-				})
+			extras.append({
+				'type': extra_type,
+				'extra': TYPE_MAP[extra_type](
+					url = 'ta://%s' % (url.lstrip('/')),
+					title = title,
+					thumb = poster
+				)
+			})
 
 		extras.sort(key=lambda e: TYPE_ORDER.index(e['type']))
 
